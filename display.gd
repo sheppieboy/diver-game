59fb13a2fbe4320d0405d886719d746f5ce75f65
Luke Sheppard <sheppiegit@gmail.com>
2023-11-27 09:51:56 -0500

    game over menu added.

--- display.gd
+++ display.gd
@@ -3,7 +3,8 @@ extends CanvasLayer
 class_name Display
 
 @onready var label = $MarginContainer/Label
-
+@onready var gv_text = $TextureRect
+@onready var gv_button = $Button
 
 func _ready():
 	label.text = "%d" % 0
@@ -11,4 +12,10 @@ func _ready():
 func update_label(gold: int):
 	label.text = "%d" % gold
 
+func on_game_over():
+	gv_text.visible = true
+	gv_button.visible = true
 
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://menu.tscn")
