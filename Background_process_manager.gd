38f0648e2c5c1e278fee132ed3d76a754679772d
Luke Sheppard <sheppiegit@gmail.com>
2023-12-01 21:24:30 -0500

    save game data and file data in file.

--- Background_process_manager.gd
+++ Background_process_manager.gd
@@ -4,6 +4,9 @@ extends Node
 @onready var npc_spawner = $NPCSpawner
 @onready var foreground = $Foreground
 @onready var display = $CanvasLayer as Display
+
+const SAVE_FILE_PATH = "res://saved.bin"
+
 var gold = 0
 
 func _ready():
@@ -26,3 +29,19 @@ func game_over():
 func collect_gold():
 	gold += 1
 	display.update_label(gold)
+
+#saves gold count for the game
+func save():
+	#write to the file
+	var game_file = FileAccess.open(SAVE_FILE_PATH, FileAccess.WRITE)
+	var dict: Dictionary = {
+		"gold": gold,
+	}
+	var jsonGameObjects = JSON.stringify(dict)
+	game_file.store_line(jsonGameObjects)
+
+#loads the game data
+func load():
+	var game_file = FileAccess.open(SAVE_FILE_PATH, FileAccess.READ)
+
+
